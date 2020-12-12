45fbf157943a67f7c44dd8ced6be155aa73e2227
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2020-12-12 19:18:48 +0530

    Updated with our model

--- actions/actions.py
+++ actions/actions.py
@@ -4,12 +4,18 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
-
+!pip install html2text simpletransformers --quiet
 # This is a simple example for a custom action which utters "Hello World!"
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from bs4 import BeautifulSoup
+import requests
+import html2text
+from markdown import markdown
+import re
+from googlesearch import search
+from simpletransformers.question_answering import QuestionAnsweringModel
 
 
 class ActionHelloWorld(Action):
@@ -20,7 +26,94 @@ class ActionHelloWorld(Action):
      def run(self, dispatcher: CollectingDispatcher,
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-         dispatcher.utter_message(text="Hello World!")
+	 model = QuestionAnsweringModel('distilbert','distilbert-base-uncased-distilled-squad')
+         recent_msg = tracker.latest_message.text
+         ans = q_to_a(model, recent_msg, n=8)
+         dispatcher.utter_message(text=ans)
 
          return []
+         
+     def predict_answer(model, question, contexts, seq_len=256, debug=False):
+       split_context = []
+    
+       if not isinstance(contexts, list):
+         contexts = [contexts]
+    
+       for context in contexts:
+         for i in range(0, len(context), seq_len):
+            split_context.append(context[i:i+seq_len])
+            
+       split_context = contexts
+    
+       f_data = []
+    
+       for i, c in enumerate(split_context):
+         f_data.append(
+            {'qas': 
+              [{'question': question,
+               'id': i,
+               'answers': [{'text': ' ', 'answer_start': 0}],
+               'is_impossible': False}],
+              'context': c
+            })
+        
+       prediction = model.predict(f_data)
+       print(type(prediction))
+       if debug:
+         print(prediction)
+       preds = []
+       preds = prediction[0][0]['answer']
+    
+    #preds = [preds[0].lower().strip() for x in prediction if preds[0].strip() != ' ']
+       if preds:
+         return sorted(list(set(preds)), key=preds.count, reverse=True)
+       return 'No answer'
+
+     def markdown_to_text(markdown_string):
+    """ Converts a markdown string to plaintext """
+
+    # md -> html -> text since BeautifulSoup can extract text cleanly
+       html = markdown(markdown_string)
+
+    # remove code snippets
+       html = re.sub(r'<pre>(.*?)</pre>', ' ', html)
+       html = re.sub(r'<code>(.*?)</code >', ' ', html)
+
+    # extract text
+       soup = BeautifulSoup(html, "html.parser")
+       text = ''.join(soup.findAll(text=True))
+
+       return text
+
+     def format_text(text):
+       text = markdown_to_text(text)
+       text = text.replace('\n', ' ')
+       return text
+
+     def query_pages(query, n=5):
+       links = list(search(ques, stop=n))
+       links = links[::-1][0:6]
+       return links
+
+     def query_to_text(query, n=5):
+       html_conv = html2text.HTML2Text()
+       html_conv.ignore_links = True
+       html_conv.escape_all = True
+    
+       text = []
+       for link in query_pages(query, n):
+        req = requests.get(link)
+        text.append(html_conv.handle(req.text))
+        text[-1] = format_text(text[-1])
+        
+       return text
+
+     def q_to_a(model, question, n=5, debug=False):
+        new_pred = []
+        opt = ''
+        context = query_to_text(question, n=n)
+        pred = list(set(predict_answer(model, question, context, debug=debug)))
+        for p in pred:
+         new_pred.append(p.capitalize())
+         opt += (p.capitalize() + '. ')
+        return opt
